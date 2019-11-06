dea887eee60d8b433451bafcb0767fd60ac870b7
decoejz <decoejz@gmail.com>
2019-11-05 23:14:15 -0300

    updaate

--- configvpn.sh
+++ configvpn.sh
@@ -15,22 +15,23 @@ source vars
 cd ~/openvpn-ca/keys
 sudo cp ca.crt server.crt server.key ta.key dh2048.pem /etc/openvpn
 gunzip -c /usr/share/doc/openvpn/examples/sample-config-files/server.conf.gz | sudo tee /etc/openvpn/server.conf
-#Substituir o arquivo /etc/openvpn/server.conf pelo da minha maquina COM SUDO
-#Substituir o arquivo /etc/sysctl.conf pelo da minha maquina COM SUDO
+sudo cp /config-openvpn/server.conf /etc/openvpn/server.conf
+sudo cp /config-openvpn/sysctl.conf /etc/sysctl.conf
 sudo sysctl -p
-#Substituir o arquivo /etc/ufw/before.rules pelo da minha maquina COM SUDO
-#Substituir o arquivo /etc/default/ufw pelo da minha maquina COM SUDO
+sudo cp /config-openvpn/before.rules /etc/ufw/before.rules
+sudo cp /config-openvpn/ufw /etc/default/ufw
 sudo ufw allow 443/tcp
 sudo ufw allow OpenSSH
+sudo ufw allow 5000/tcp
 sudo ufw disable
-sudo ufw enable -y
+sudo ufw enable #Responder 'y'
 sudo systemctl start openvpn@server
 sudo systemctl enable openvpn@server
 mkdir -p ~/client-configs/files
 chmod 700 ~/client-configs/files
 cp /usr/share/doc/openvpn/examples/sample-config-files/client.conf ~/client-configs/base.conf
-#Substituir o arquivo ~/client-configs/base.conf pelo da minha maquina
-#Passar o arquivo make_config.sh para dentro de ~/client-configs/
+cp /config-openvpn/base.conf ~/client-configs/base.conf
+cp /config-openvpn/make_config.sh ~/client-configs/make_config.sh
 chmod 700 ~/client-configs/make_config.sh
 cd ~/client-configs
 ./make_config.sh client1
